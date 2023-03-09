60dd7f651fcb8d6fc422474c2e9df334fd293800
simdev1234 <simdev1234@gmail.com>
2023-03-09 15:42:30 +0900

    feat : 팔로우 기능

--- src/main/java/com/ecloth/beta/follow/controller/FollowController.java
+++ src/main/java/com/ecloth/beta/follow/controller/FollowController.java
@@ -4,22 +4,24 @@ import com.ecloth.beta.follow.dto.FollowList;
 import com.ecloth.beta.follow.dto.Following;
 import com.ecloth.beta.follow.exception.FollowException;
 import com.ecloth.beta.follow.service.FollowService;
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
-
 import java.security.Principal;
-
 import static com.ecloth.beta.follow.exception.ErrorCode.FOLLOW_REQUEST_NOT_VALID;
 import static com.ecloth.beta.follow.exception.ErrorCode.UNFOLLOW_REQUEST_NOT_VALID;
 
 @RestController
-@RequestMapping("/api/member/follow")
+@RequiredArgsConstructor
+@RequestMapping("/api/member")
+@Api(tags = "팔로우 API")
 public class FollowController {
 
-    private FollowService followService;
+    private final FollowService followService;
 
-    @PostMapping
+    @PostMapping("/follow")
     public ResponseEntity<Following.Response> follow(@ApiIgnore Principal principal,
                                     @RequestBody Following.Request request){
 
@@ -33,7 +35,7 @@ public class FollowController {
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping
+    @GetMapping("/follow")
     public ResponseEntity<Following.Response> getFollowStatus(@ApiIgnore Principal principal,
                                              @RequestParam Long targetId){
 
@@ -42,7 +44,7 @@ public class FollowController {
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping
+    @GetMapping("/follows")
     public ResponseEntity<FollowList.Response> getFollowList(@ApiIgnore Principal principal,
                                            FollowList.Request request){
 
@@ -51,9 +53,9 @@ public class FollowController {
         return ResponseEntity.ok(response);
     }
 
-    @DeleteMapping
+    @DeleteMapping("/follow")
     public ResponseEntity<Following.Response> unfollow(@ApiIgnore Principal principal,
-                                      @RequestBody Following.Request request){
+                                                        @RequestBody Following.Request request){
 
         if (request.isFollowStatus() == true) {
             throw new FollowException(UNFOLLOW_REQUEST_NOT_VALID);
